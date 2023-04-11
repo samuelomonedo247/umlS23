a045fd5794c779e3a957bc05c24ff638b6ef653c
Dan Lambright <dlambrig@gmail.com>
2023-04-11 08:08:32 -0400

    week12 material

---
+++ week12/extract_mysql_full.py
@@ -0,0 +1,37 @@
+import pymysql
+import csv
+import configparser
+
+parser = configparser.ConfigParser()
+parser.read("pipeline.conf")
+hostname = parser.get("mysql_config", "hostname")
+port = parser.get("mysql_config", "port")
+username = parser.get("mysql_config", "username")
+dbname = parser.get("mysql_config", "database")
+password = parser.get("mysql_config", "password")
+
+conn = pymysql.connect(host=hostname,
+        user=username,
+        password=password,
+        db=dbname,
+        port=int(port))
+
+if conn is None:
+  print("Error connecting to the MySQL database")
+else:
+  print("MySQL connection established!")
+
+  m_query = "SELECT * FROM Orders;"
+local_filename = "order_extract.csv"
+
+m_cursor = conn.cursor()
+m_cursor.execute(m_query)
+results = m_cursor.fetchall()
+
+with open(local_filename, 'w') as fp:
+  csv_w = csv.writer(fp, delimiter='|')
+  csv_w.writerows(results)
+
+fp.close()
+m_cursor.close()
+conn.close()
